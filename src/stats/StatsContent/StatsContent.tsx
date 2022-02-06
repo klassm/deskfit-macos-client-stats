ab4e638b2cbec5843df9de200a63ecc99d56a51c
Matthias Klass <matthias.klass@tngtech.com>
2022-02-06 13:34:15 +0100

    More stats

--- src/stats/StatsContent/StatsContent.tsx
+++ src/stats/StatsContent/StatsContent.tsx
@@ -1,31 +1,18 @@
-import { maxBy } from "lodash";
 import { DateTime } from "luxon";
 import { FC, useMemo } from "react";
-import { Entry } from "../../fetchData";
-import { AverageStatsBox } from "../statsBoxes/AverageStepsBox/AverageStatsBox";
-import { DistanceStatsBox} from "../statsBoxes/DistanceStatsBox/DistanceStatsBox";
 import { ByDayGraph } from "../../ByDayGraph/ByDayGraph";
+import { ByMonthGraph } from "../../ByMonthGraph/ByMonthGraph";
+import { Entry } from "../../fetchData";
 import { StatsBar } from "../StatsBar/StatsBar";
-import { DistanceStatsBoxEntry } from "../../utils/DistanceStatsBoxEntry";
+import { AverageStatsBox } from "../statsBoxes/AverageStepsBox/AverageStatsBox";
 import { TopDayStatsBox } from "../statsBoxes/TopDayStatsBox/TopDayStatsBox";
 import { TotalStatsBox } from "../statsBoxes/TotalStatsBox/TotalStatsBox";
 import { StatsGroup } from "../StatsGroup/StatsGroup";
-import { ByMonthGraph } from "../../ByMonthGraph/ByMonthGraph";
 
 export interface Props {
   data: Entry[]
   month: DateTime
 }
-
-
-function sumFor(date: DateTime | undefined, data: Entry[]): DistanceStatsBoxEntry & { date: DateTime | undefined } {
-  return data.reduce((prev, cur) => ( {
-    ...prev,
-    meters: prev.meters + cur.meters,
-    steps: prev.steps + cur.steps
-  } ), { date, meters: 0, steps: 0 })
-}
-
 export const StatsContent: FC<Props> = ({ data, month: selectedMonth }) => {
   const monthData = useMemo(() =>
       data.filter(entry => entry.date.year === selectedMonth.year && entry.date.month === selectedMonth.month),
@@ -34,9 +21,6 @@ export const StatsContent: FC<Props> = ({ data, month: selectedMonth }) => {
 
   const yearData = useMemo(() => data.filter(entry => entry.date.year === selectedMonth.year), [selectedMonth, data]);
 
-  const overallYear = useMemo(() => sumFor(selectedMonth.startOf("year"), yearData), [yearData, selectedMonth]);
-  const overallMonth = useMemo(() => sumFor(selectedMonth, monthData), [monthData, selectedMonth]);
-  const overall = useMemo(() => sumFor(undefined, data), [data]);
   const thirtyDaysAgo = DateTime.now().minus({days: 30});
   const last30Days = data.filter(({date}) => date.toMillis() > thirtyDaysAgo.toMillis());
 
